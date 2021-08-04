be8f20a9b454b54ad44bd2fd61ed3c5aad7bf284
Vladislav <vlad.murashchenko@gmail.com>
2021-08-04 12:41:50 +0300

    initialize app

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import GamePage from "./pages/GamePage";
+
+function App() {
+  return <GamePage />;
+}
+
+export default App;
---
+++ src/pages/GamePage/GamePage.tsx
@@ -0,0 +1,5 @@
+const Game = () => {
+  return <div>Hello!</div>;
+};
+
+export default Game;
